6553870b4b1a615cf40fbab3eaecb955eeecd85e
nicky <362330721@qq.com>
2020-12-31 15:20:02 +0800

    新增SpringCloud Alibaba Nacos配置例子

--- springcloud-alibaba-nacos-config/src/main/java/com/example/springcloudalibaba/nacosconfig/SpringcloudAlibabaNacosConfigApplication.java
+++ springcloud-alibaba-nacos-config/src/main/java/com/example/springcloudalibaba/nacosconfig/SpringcloudAlibabaNacosConfigApplication.java
@@ -1,10 +1,21 @@
 package com.example.springcloudalibaba.nacosconfig;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
 public class SpringcloudAlibabaNacosConfigApplication {
+
+    @Value("${user.name}")
+    private String userName;
+
+    @PostConstruct
+    public void init() {
+       System.out.println(String.format("从Nacos配置中心获取数据,username:%s",userName));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringcloudAlibabaNacosConfigApplication.class, args);
